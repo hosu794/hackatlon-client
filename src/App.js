d774f1a3860686c96ac5a00384704124df67a666
Grzegorz Szczesny <grzeszczesny14@gmail.com>
2020-10-15 17:49:02 +0200

    Create a dev.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>beHealth App</h1>
+      <p>Elo</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab aperiam debitis voluptatibus. Minus porro odio accusantium saepe deserunt nulla at.</p>
     </div>
   );
 }
