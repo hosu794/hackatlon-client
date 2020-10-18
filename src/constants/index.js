0cbc1a69d4050674a491cb76b3f3c976f6450e90
Grzegorz Szczesny <grzeszczesny14@gmail.com>
2020-10-18 10:35:06 +0200

    Create a constants and start creating a services and redux .

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export * from './alert.constants'
+export * from './auth.contants'
+export * from './user.constants'
---
+++ src/services/authentication.service.js
@@ -0,0 +1,5 @@
+import {authHeader } from '../utils'
+
+import axios from 'axios'
+
+export const authService = {}
