e1aa7befb94fe14697ee6c5542cf2371a7b98fa7
SZYBKIDAMIAN <72219970+SZYBKIDAMIAN@users.noreply.github.com>
2020-10-21 17:16:50 +0200

    Kategorie (wstępnie)

--- src/components/path/PathItem.js
+++ src/components/path/PathItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 const PathItem = ({title}) => {
     return (
         <div className="K1">
-        <h1 >{title}</h1>
+        <h1 id="tytul-kategoria" >{title}</h1>
     </div>
     )
 }
