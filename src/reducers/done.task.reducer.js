d577d2fdc7c59af6aa385adf39ce07caa58e16b6
hosu794 <grzesszczesny14@gmail.com>
2020-10-21 16:36:41 +0200

    Create a donetask reducer and task reducer.

---
+++ src/reducers/done.task.reducer.js
@@ -0,0 +1,49 @@
+import { donetaskConstants } from '../constants'
+
+export function donetask(state = {}, action) {
+    switch (action.type) {
+        case donetaskConstants.CREATE_DONE_TASK_REQUEST:
+        case donetaskConstants.DELETE_DONE_TASK_REQUEST:
+        case donetaskConstants.GET_CURRENT_LEVEL_REQUEST:
+        case donetaskConstants.GET_DONETASKS_BY_CREATED_BY_REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+        case donetaskConstants.DELETE_DONE_TASK_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                content: state.content.filter((val) => val.id !== action.index),
+            }
+        case donetaskConstants.GET_DONETASKS_BY_CREATED_BY_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                content: state.content.concat(action.data)
+            }
+        case donetaskConstants.GET_CURRENT_LEVEL_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                currentLevel: action.data
+            }
+        case donetaskConstants.GET_DONETASKS_BY_CREATED_BY_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                content: action.data.content
+            }
+        case donetaskConstants.GET_CURRENT_LEVEL_FAILURE:
+        case donetaskConstants.GET_DONETASKS_BY_CRAETED_BY_FAILURE:
+        case donetaskConstants.CREATE_DONE_TASK_FAILURE:
+        case donetaskConstants.DELETE_DONE_TASK_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error: action.error
+            }
+        default:
+            return state;
+    }
+}
