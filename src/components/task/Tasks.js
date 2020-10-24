a9a4426787ca8a57aa79194fff429847119b7a5b
hosu794 <grzesszczesny14@gmail.com>
2020-10-24 19:04:47 +0200

    Add path name to Tasks file .

--- src/components/task/Tasks.js
+++ src/components/task/Tasks.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react'
-import {taskActions} from '../../actions'
+import {pathActions, taskActions} from '../../actions'
 import {useDispatch, useSelector} from 'react-redux'
 import TaskItem from './TaskItem';
 import PropTypes from 'prop-types'
@@ -9,14 +9,20 @@ function Tasks(props) {
     const dispatch = useDispatch();
 
     const content = useSelector(state => state.task.content);
+    const path = useSelector(state => state.path.currentPath);
 
     useEffect(() => {
         dispatch(taskActions.getTasksTodoByPath(props.match.params.id));
+       
     }, [content])
+
+    useEffect(() => {
+        dispatch(pathActions.getPathById(props.match.params.id));
+    }, [])
 
     return (
         <div className="napis-zadania"> 
-            <h1>Zadania</h1>
+            <h1>{path ? path.title : "Ładowanie..." }</h1>
             {content ? content.map(item => (
                <TaskItem  key={item.id} title={item.title} id={item.id} />
             )) : "Brak dostępnych zadań dla kategorii"}
